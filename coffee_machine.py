15aaef86ee8caa368d64f9b15c0688f7837708c7
Damsith Wijekoon <damsith.wijekoon@Outlook.com>
2023-02-04 16:21:40 +0530

    Updated coffee_machine

--- coffee_machine.py
+++ coffee_machine.py
@@ -13,14 +13,44 @@ def show_report():
 
     
 def update_report(water, milk, coffee, money):
+    """Updates the amounts the resources we have"""
+    global resources
     resources['Water']['amount'] -= water
     resources['Milk']['amount'] -= milk
     resources['Coffee']['amount'] -= coffee
     resources['Money']['amount'] += money
 
 
-show_report()
-update_report(100, 100, 50, 100)
-show_report()
+def process_coins(quarters, dimes, nickles, pennies):
+    money = quarters * 0.25 + dimes * 0.10 + nickles * 0.05 + pennies * 0.01
+    return money
+
+
+def check_resources(wanted_water, wanted_milk, wanted_coffee):
+    not_enough = []
+    left_water = resources['Water']['amount']
+    left_milk = resources['Milk']['amount']
+    left_coffee = resources['Coffee']['amount']
+    
+    if left_water < wanted_water:
+        not_enough.append('water')
+    if left_milk < wanted_milk:
+        not_enough.append('milk')
+    if left_coffee < wanted_coffee:
+        not_enough.append('coffee')
+    return not_enough
+
+
+def check_trans(paid, wanted_amount):
+    balance = 0
+    if paid > wanted_amount:
+        balance = paid - wanted_amount
+        return balance
+    elif paid < wanted_amount:
+        return 'bad'
+    else:
+        return balance
+
+
 # while True:
 #     user = input('What would you like? (espresso, latte, cappuccino): ')
