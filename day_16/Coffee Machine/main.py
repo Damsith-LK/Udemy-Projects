fd34e40dd9ca6095e132998bd5e26aec21524753
Damsith Wijekoon <damsith.wijekoon@Outlook.com>
2023-02-08 13:14:35 +0530

    Testing commit

--- day_16/Coffee Machine/main.py
+++ day_16/Coffee Machine/main.py
@@ -20,4 +20,4 @@ while is_on:
         drink = menu.find_drink(choice)
         
         if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
-            coffee_maker.make_coffee(drink)
+            coffee_maker.make_coffee(drink)  # Testing commit here
