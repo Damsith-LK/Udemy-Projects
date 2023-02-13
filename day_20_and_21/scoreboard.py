3179e9314245c6ca39d80c02e972bd95001c61b3
Damsith Wijekoon <damsith.wijekoon@Outlook.com>
2023-02-13 13:18:15 +0530

    Create the scoreboard

---
+++ day_20_and_21/scoreboard.py
@@ -0,0 +1,19 @@
+from turtle import Turtle
+
+
+class ScoreBoard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color('white')
+        self.penup()
+        self.speed('fastest')
+        self.goto(x=0, y=310)
+        self.hideturtle()
+
+    def update_score(self):
+        self.score += 1
+        self.clear()
+
+    def display_score(self):
+        self.write(arg=f'Score: {self.score}', move=False, align='center', font=('Ariel', 25, 'bold'))
