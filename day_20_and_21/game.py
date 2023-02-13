9a064afde96a8910a505602f3139c735a0a7775d
Damsith Wijekoon <damsith.wijekoon@Outlook.com>
2023-02-13 14:08:17 +0530

    Complete snake game

--- day_20_and_21/game.py
+++ day_20_and_21/game.py
@@ -5,16 +5,19 @@ Here are the steps:
 2. Move the snake - Done
 3. Control the snake (Using event listeners) - Done
 4. Detect collision with food - Done
-5. Scoreboard
-6. Detect collision with wall (game over)
-7. Detect collision with tail (game over)
+5. Scoreboard - Done
+6. Detect collision with wall (game over) - Done
+7. Detect collision with tail (game over) - Done
+8. Extend the snake - Done
 """
 
 import turtle
 from snake import Snake
 from food import Food
+from scoreboard import ScoreBoard
 from time import sleep
 
+turtle.colormode(255)
 screen = turtle.Screen()
 screen.title('Snake Game')
 screen.tracer(0)
@@ -23,6 +26,7 @@ screen.setup(width=1000, height=700)
 
 snake = Snake()
 food = Food()
+scoreboard = ScoreBoard()
 
 screen.listen()
 screen.onkey(snake.up, 'Up')
@@ -33,6 +37,7 @@ screen.onkey(snake.left, 'Left')
 game_continue = True
 
 while game_continue:
+    scoreboard.display_score()
     screen.update()
     sleep(0.1)
     snake.move()
@@ -40,6 +45,21 @@ while game_continue:
     # Detect collision with food
     if snake.head.distance(food) < 15:
         food.new_food()
+        snake.extend()
+        scoreboard.update_score()
+
+    # Detect collision with wall
+    if snake.head.xcor() > 490 or snake.head.xcor() < -490 or snake.head.ycor() > 340 or snake.head.ycor() < -340:
+        game_continue = False
+        scoreboard.game_over()
+
+    # Detect collision with tail
+    for i in snake.segments:
+        if i == snake.head:
+            continue
+        if snake.head.distance(i) < 10:
+            game_continue = False
+            scoreboard.game_over()
 
 
 screen.exitonclick()
