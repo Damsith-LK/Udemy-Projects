45db200eda9a07d2d8db7691479a58d2550c4d9c
Damsith Wijekoon <damsith.wijekoon@Outlook.com>
2023-02-13 14:07:31 +0530

    Add new methods so that the snake can extend now

--- day_20_and_21/snake.py
+++ day_20_and_21/snake.py
@@ -3,6 +3,7 @@
 import turtle
 
 STARTING_POSITIONS = [(0,0), (-20, 0), (-40, 0)]
+turtle.colormode(225)
 
 
 class Snake:
@@ -13,17 +14,25 @@ class Snake:
 
     def create_snake(self):
         for position in STARTING_POSITIONS:
-            new_seg = turtle.Turtle("square")
-            new_seg.color("white")
-            new_seg.penup()
-            new_seg.goto(position)
-            self.segments.append(new_seg)
+            self.create_segments(position)
+    # These two methods create the snake body
+
+    def create_segments(self, pos):
+        new_seg = turtle.Turtle("square")
+        new_seg.color("white")
+        new_seg.penup()
+        new_seg.goto(pos)
+        self.segments.append(new_seg)
+
+    def extend(self):
+        self.create_segments(self.segments[-1].position())
 
     def move(self):
         for n_seg in range(len(self.segments) - 1, 0, -1):
             new_x = self.segments[n_seg - 1].xcor()
             new_y = self.segments[n_seg - 1].ycor()
             self.segments[n_seg].goto(new_x, new_y)
+        self.head.color((200, 200, 200))
         self.head.forward(20)
 
     def up(self):
