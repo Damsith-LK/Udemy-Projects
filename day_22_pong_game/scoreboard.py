e9fd3ba9ec92263368dcf9380886c9f260416dfa
Damsith Wijekoon <damsith.wijekoon@Outlook.com>
2023-02-14 13:18:05 +0530

    Create the scoreboard

---
+++ day_22_pong_game/scoreboard.py
@@ -0,0 +1,17 @@
+from turtle import Turtle
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.right_score = 0
+        self.left_score = 0
+        self.speed('fastest')
+        self.penup()
+        self.color('white')
+        self.goto(0, 275)
+        self.hideturtle()
+
+    def display_score(self):
+        self.write(arg=f'{self.left_score}         {self.right_score}', align='center', move=False, font=('Courier', 50, 'bold'))
