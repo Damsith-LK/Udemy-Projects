298784235db5852bb77ebe3d191e99a6503b106c
Damsith Wijekoon <damsith.wijekoon@Outlook.com>
2023-03-02 13:54:55 +0530

    Design the behaviour of cars

--- day_23_turtle_crossing/cars.py
+++ day_23_turtle_crossing/cars.py
@@ -1,18 +1,29 @@
 from turtle import Turtle
-from random import randint
+from random import randint, choice
+
+COLORS = ['orange', 'yellow', 'cyan', 'blue', 'green', 'brown', 'red', 'purple', 'pink']
+STARTING_SPEED = 5
 
 
 class Cars(Turtle):
 
     def __init__(self):
-        self.level = 1
         super().__init__()
-        self.speed(0)
-        self.penup()
-        self.shape('square')
-        self.color((randint(0, 255), randint(0, 255), randint(0, 255)))
-        self.shapesize(stretch_wid=1, stretch_len=2)
-        self.goto(280, randint(-280, 280))
+        self.cars = []
+        self.color('white')
+
+    def create_cars(self):
+        rand = randint(1, 6)
+        if rand == 1:
+            new_car = Turtle()
+            new_car.speed('fastest')
+            new_car.penup()
+            new_car.shape('square')
+            new_car.shapesize(stretch_len=2, stretch_wid=1)
+            new_car.color(choice(COLORS))
+            new_car.goto(300, randint(-280, 280))
+            self.cars.append(new_car)
 
     def move(self):
-        self.backward(20)
+        for car in self.cars:
+            car.backward(STARTING_SPEED)
