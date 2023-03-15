2d650cc7a21b1b79483816316cb38541e1832036
Damsith Wijekoon <damsith.wijekoon@Outlook.com>
2023-03-15 14:10:01 +0530

    Start of the squirrel count challenge

---
+++ day_25/squirral_census/main.py
@@ -0,0 +1,7 @@
+# Figure about how many grey, cinnamon, black squirrels are there in total (Primary fur color)
+# Save the data into a .csv file named "squirrel_count.csv"
+
+import pandas as pd
+
+data = pd.read_csv('2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv')
+print(data)
