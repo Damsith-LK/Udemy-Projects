12df0bfe84f766e598b3dbacc0ac4a372cc61dcb
Damsith Wijekoon <damsith.wijekoon@Outlook.com>
2023-03-21 19:09:03 +0530

    Start of day 27

---
+++ day_27/args_and_kwargs/optional_arg.py
@@ -0,0 +1,7 @@
+# Here's how you set optional arguments
+
+def func(arg1=1, arg2=2, arg3=3):
+    pass
+
+
+func()  # Here we don't need to enter the arguments if not necessary, since the arguments are already set to a default value when the function was implemented
