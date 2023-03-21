77aceab5ddd48309ed9f4d2713dd1af6f0d53ba1
Damsith Wijekoon <damsith.wijekoon@Outlook.com>
2023-03-21 17:46:31 +0530

    Start NATO Alphabet challenge

---
+++ day_26-NATO_alphabet/main.py
@@ -0,0 +1,18 @@
+import pandas
+
+student_dict = {
+    "student": ["Angela", "James", "Lily"], 
+    "score": [56, 76, 98]
+}
+
+# Looping through dictionaries:
+for (key, value) in student_dict.items():
+    # Access key and value
+    pass
+
+
+student_data_frame = pandas.DataFrame(student_dict)
+
+nato_alphabet = {index: row for (index, row) in student_data_frame.iterrows()}
+print(nato_alphabet)
+
