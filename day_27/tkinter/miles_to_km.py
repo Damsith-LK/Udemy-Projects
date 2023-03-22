fbbf6a47ae1cb7305637eb4e44442a90a112e8b4
Damsith Wijekoon <damsith.wijekoon@Outlook.com>
2023-03-22 13:40:47 +0530

    Start the challenge miles to km

---
+++ day_27/tkinter/miles_to_km.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+
+# creating the window
+window = tk.Tk()
+window.title("Miles to Kilo Meters Converter")
+window.eval('tk::PlaceWindow . center')
+window.minsize(height=200, width=300)
+window.maxsize(height=200, width=300)
+
+
+
+
+window.mainloop()
