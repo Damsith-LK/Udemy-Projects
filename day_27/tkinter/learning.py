6cf0160e6f6c3737f899c826c3a43d1ccb7d6e42
Damsith Wijekoon <damsith.wijekoon@Outlook.com>
2023-03-22 11:54:26 +0530

    Most basic Tkinter

--- day_27/tkinter/learning.py
+++ day_27/tkinter/learning.py
@@ -8,6 +8,26 @@ window.maxsize(width=500, height=500)
 
 # Creating a label on the window
 label = tk.Label(text="This is a label", font=("Times New Roman", 30, "bold"))
-label.pack(side="left")
+label.pack()
+
+# Changing the label
+label.config(text="Label Updated.")
+
+# Buttons
+button = tk.Button(text="Click this button")
+button.pack()
+
+# Entry (Getting inputs)
+entry = tk.Entry()
+entry.pack()
+
+
+def button_clicked():
+    inp = entry.get()
+    label.config(text=inp)
+
+
+button.config(command=button_clicked)
+
 
 window.mainloop()
