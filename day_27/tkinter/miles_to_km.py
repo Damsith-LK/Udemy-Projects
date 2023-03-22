0e27e925d2c00c3bbff093f5c828fd5d6fcc3011
Damsith Wijekoon <damsith.wijekoon@Outlook.com>
2023-03-22 14:17:33 +0530

    Complete the miles to km challenge and day 27

--- day_27/tkinter/miles_to_km.py
+++ day_27/tkinter/miles_to_km.py
@@ -22,4 +22,18 @@ km_label = tk.Label(text='km', font=("Courier", 15, 'bold'))
 km_label.place(x=300, y=100, anchor="center")
 
 
+def cal():
+    kilo_label.config(text=round(float(entry.get()) * 1.60934, 2))
+
+
+# The entry field here
+entry = tk.Entry()
+entry.place(x=200, y=50, anchor='center', width=100, height=30)
+
+# The button "Calculate"
+button = tk.Button(text='Calculate')
+button.place(x=200, y=150, anchor='center')
+button.config(command=cal)
+
+
 window.mainloop()
