a20d4c9c56cddea43a733c6b82181b3194ed563e
Damsith Wijekoon <damsith.wijekoon@Outlook.com>
2023-03-25 15:42:16 +0530

    Complete password saving

--- day_29-password_manager/main.py
+++ day_29-password_manager/main.py
@@ -4,6 +4,18 @@ from tkinter import *
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
+def save_password():
+    website = website_entry.get()
+    email_ = email_entry.get()
+    password = password_entry.get()
+
+    with open("data.txt", "a") as file:
+        file.write(f"{website} | {email_} | {password}\n")
+
+    website_entry.delete(0, END)
+    password_entry.delete(0, END)
+    website_entry.focus()
+
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password Manager")
@@ -23,14 +35,20 @@ password_label.grid(row=3, column=0, sticky="e")
 
 website_entry = Entry(width=35)
 website_entry.grid(row=1, column=1, columnspan=3, padx=5, pady=5, sticky="w")
+website_entry.focus()
+
 email_entry = Entry(width=35)
 email_entry.grid(row=2, column=1, columnspan=3, padx=5, pady=5, sticky="w")
+with open('email.txt', 'r') as file:
+    email = file.read()
+email_entry.insert(0, email)
+
 password_entry = Entry(width=35)
 password_entry.grid(row=3, column=1, padx=5, pady=5, sticky="w")
 
 generate_password_button = Button(text="Generate Password")
 generate_password_button.grid(row=3, column=2, padx=5, pady=5)
-add_button = Button(text="Add", width=36)
+add_button = Button(text="Add", width=36, command=save_password)
 add_button.grid(row=4, column=1, columnspan=2, padx=5, pady=5)
 
 window.mainloop()
