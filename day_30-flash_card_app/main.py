1f89b251a2221083bfebdda6b82125538c623388
Damsith Wijekoon <damsith.wijekoon@Outlook.com>
2023-03-27 16:46:52 +0530

    Start day 30 - Flash Card App

---
+++ day_30-flash_card_app/main.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+BACKGROUND_COLOR = "#B1DDC6"
+
+
+# -------------------------- UI Setup ------------------------------ #
+window = Tk()
+window.title("Flash Card App")
+
+window.mainloop()
