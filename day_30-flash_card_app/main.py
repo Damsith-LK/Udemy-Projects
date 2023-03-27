b976d7e1ef8ce42a0b31864c45f9db1c25f4d849
Damsith Wijekoon <damsith.wijekoon@Outlook.com>
2023-03-27 17:16:04 +0530

    Complete the UI

--- day_30-flash_card_app/main.py
+++ day_30-flash_card_app/main.py
@@ -6,5 +6,24 @@ BACKGROUND_COLOR = "#B1DDC6"
 # -------------------------- UI Setup ------------------------------ #
 window = Tk()
 window.title("Flash Card App")
+window.config(pady=50, padx=50, bg=BACKGROUND_COLOR)
+
+back_img = PhotoImage(file="./images/card_back.png")
+front_img = PhotoImage(file="./images/card_front.png")
+right_img = PhotoImage(file="./images/right.png")
+wrong_img = PhotoImage(file="./images/wrong.png")
+
+canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
+win_img = canvas.create_image(400, 263, image=front_img)
+title = canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
+word = canvas.create_text(400, 263, text="Word", font=("Ariel", 60, "bold"))
+
+canvas.grid(column=0, row=0, columnspan=2)
+
+wrong_button = Button(image=wrong_img, highlightthickness=0, bg=BACKGROUND_COLOR, borderwidth=0)
+right_button = Button(image=right_img, highlightthickness=0, bg=BACKGROUND_COLOR, borderwidth=0)
+wrong_button.grid(row=1, column=0)
+right_button.grid(row=1, column=1)
+
 
 window.mainloop()
