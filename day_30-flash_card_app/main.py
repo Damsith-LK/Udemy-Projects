c37bf67349d8b10296991ebb91941d2af4db27bc
Damsith Wijekoon <damsith.wijekoon@Outlook.com>
2023-03-28 17:46:33 +0530

    Code the part where random french words gets displayed when the button is pressed

--- day_30-flash_card_app/main.py
+++ day_30-flash_card_app/main.py
@@ -1,6 +1,16 @@
 from tkinter import *
+import pandas as pd
+from random import choice
 
 BACKGROUND_COLOR = "#B1DDC6"
+
+# ---------------------- Reading the csv file and associating with the GUI --------------------------- #
+data = pd.read_csv("french_words.csv")
+french_words = data.French.to_list()
+
+def french_word_display():
+    rand_word = choice(french_words)
+    canvas.itemconfig(word, text=rand_word)
 
 
 # -------------------------- UI Setup ------------------------------ #
@@ -15,13 +25,13 @@ wrong_img = PhotoImage(file="./images/wrong.png")
 
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 win_img = canvas.create_image(400, 263, image=front_img)
-title = canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
+title = canvas.create_text(400, 150, text="French", font=("Ariel", 40, "italic"))
 word = canvas.create_text(400, 263, text="Word", font=("Ariel", 60, "bold"))
 
 canvas.grid(column=0, row=0, columnspan=2)
 
-wrong_button = Button(image=wrong_img, highlightthickness=0, bg=BACKGROUND_COLOR, borderwidth=0)
-right_button = Button(image=right_img, highlightthickness=0, bg=BACKGROUND_COLOR, borderwidth=0)
+wrong_button = Button(image=wrong_img, highlightthickness=0, bg=BACKGROUND_COLOR, borderwidth=0, command=french_word_display)
+right_button = Button(image=right_img, highlightthickness=0, bg=BACKGROUND_COLOR, borderwidth=0, command=french_word_display)
 wrong_button.grid(row=1, column=0)
 right_button.grid(row=1, column=1)
 
