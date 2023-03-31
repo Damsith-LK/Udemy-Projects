b97e12edc5df06045b484813f705043a5598707c
Damsith Wijekoon <damsith.wijekoon@Outlook.com>
2023-03-31 17:11:14 +0530

    Send email with SMTP (May be a spam)

--- day_32/smtp.py
+++ day_32/smtp.py
@@ -1,5 +1,11 @@
 # SMTP = Simple Mail Transfer Protocol
+# The email I'm going to send emails, is a gmail
 
 import smtplib as smtp
+from info import *
 
-conn = smtp.SMTP()
+conn = smtp.SMTP("smtp.gmail.com")
+conn.starttls()  # Makes the connection secure
+conn.login(user=my_email, password=password)  # Logging to the account
+conn.sendmail(from_addr=my_email, to_addrs=send_email, msg="Hello")
+conn.close()
