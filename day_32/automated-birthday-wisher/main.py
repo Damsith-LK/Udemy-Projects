91a09c0edc59c0d21e6bc8b74e93cfb3113880ab
Damsith Wijekoon <damsith.wijekoon@Outlook.com>
2023-04-01 10:28:47 +0530

    Start automated-birthday-wisher project

---
+++ day_32/automated-birthday-wisher/main.py
@@ -0,0 +1,25 @@
+##################### Extra Hard Starting Project ######################
+
+# 1. Update the birthdays.csv
+
+# 2. Check if today matches a birthday in the birthdays.csv
+
+# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
+
+# 4. Send the letter generated in step 3 to that person's email address.
+
+
+import smtplib as smtp
+from random import choice
+import pandas as pd
+import datetime as dt
+
+birthday_data = pd.read_csv("birthdays.csv")
+bd_list = birthday_data.to_dict(orient="records")
+print(bd_list)
+
+now = dt.datetime.now()
+year = now.year
+month = now.month
+date = now.day
+print(year, month, date)
