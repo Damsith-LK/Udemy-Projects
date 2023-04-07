1e841c1f8c76f7090932fefb77fa90c10b317a70
Damsith Wijekoon <damsith.wijekoon@Outlook.com>
2023-04-07 15:35:06 +0530

    Complete the 1st step

--- day_36-stock-trading-alert/main.py
+++ day_36-stock-trading-alert/main.py
@@ -12,3 +12,13 @@ print(data)
 
 yesterday = str(datetime.datetime.today() - datetime.timedelta(days=1)).split(" ")[0]
 day_before = str(datetime.datetime.today() - datetime.timedelta(days=2)).split(" ")[0]
+
+yesterday_data = float(data[yesterday]["4. close"])
+day_before_data = float(data[day_before]["4. close"])
+
+percent_change = round(((yesterday_data - day_before_data) / yesterday_data) * 100, 4)
+
+if percent_change < -5 or percent_change > 5:
+    print("Get News")
+else:
+    print("Nah need of news")
