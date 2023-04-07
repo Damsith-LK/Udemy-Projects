b36be814f6ecd44dd2e723b49935408fca48235f
Damsith Wijekoon <damsith.wijekoon@Outlook.com>
2023-04-07 15:15:26 +0530

    Get the data and define the dates of yesterday and day before yesterday

--- day_36-stock-trading-alert/main.py
+++ day_36-stock-trading-alert/main.py
@@ -1,2 +1,14 @@
+import requests
+import config
+import datetime
+
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
+
+response = requests.get(f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={STOCK}&apikey={config.key}")
+response.raise_for_status()
+data = response.json()["Time Series (Daily)"]
+print(data)
+
+yesterday = str(datetime.datetime.today() - datetime.timedelta(days=1)).split(" ")[0]
+day_before = str(datetime.datetime.today() - datetime.timedelta(days=2)).split(" ")[0]
