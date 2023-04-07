11da92cc25faebded691ae2ec97e78e8d694c1b8
Damsith Wijekoon <damsith.wijekoon@Outlook.com>
2023-04-07 16:59:42 +0530

    Complete most of the Stock Trading Alert Program

--- day_36-stock-trading-alert/main.py
+++ day_36-stock-trading-alert/main.py
@@ -1,6 +1,10 @@
+# When this program is created, I didn't have access to SMS services so this one is email based
+# But later I'm going to make a SMS version of this in features branch
+
 import requests
 import config
 import datetime
+import smtplib as smtp
 
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
@@ -19,6 +23,17 @@ day_before_data = float(stock_data[day_before]["4. close"])
 percent_change = round(((yesterday_data - day_before_data) / yesterday_data) * 100, 4)
 
 
+def send_email(subject: str, body: str, author: str):
+    with smtp.SMTP("smtp.gmail.com") as conn:
+        conn.starttls()
+        conn.login(user=config.my_email, password=config.password)
+        conn.sendmail(from_addr=config.my_email, to_addrs=config.send_email, msg=
+                      f"Subject:{subject}"
+                      "\n\n"
+                      f"{body}\n-{author}"
+                      )
+
+
 def get_news() -> list:
     news_list = []
     news_response = requests.get(f"https://newsapi.org/v2/everything?q=TSLA&from={day_before}&sortBy=popularity&apiKey={config.news_key}")
@@ -27,7 +42,7 @@ def get_news() -> list:
     count = 0
 
     for i in news_data["articles"]:
-        data = {i["title"]: i["description"]}
+        data = {"title": i["title"], "description": i["description"], "author": i["author"]}
         news_list.append(data)
         count += 1
         if count > 2:
@@ -36,6 +51,14 @@ def get_news() -> list:
 
 
 if percent_change < -5 or percent_change > 5:
-    print(get_news())
+    data = get_news()
+    for ii in data:
+        send_email(ii["title"], ii["description"], ii["author"])
 else:
     print("Nah need of news")
+
+
+# Testing:
+# test_data = get_news()
+# for iii in test_data:
+#     send_email(iii["title"], iii["description"], iii["author"])
