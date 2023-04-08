6e23972f422b0819d384c67d2d89369743b903cc
Damsith Wijekoon <damsith.wijekoon@Outlook.com>
2023-04-08 17:20:14 +0530

    Create the graph (Meditation Graph)

--- day_37-habit-tracker/main.py
+++ day_37-habit-tracker/main.py
@@ -1,4 +1,5 @@
 # In this program, we'll be using not only get() but also other methods - post(), put(), delete()
+# I'm using this to keep track of my meditation
 
 import requests
 import config
@@ -6,11 +7,24 @@ import config
 PIXELA_ENDPOINT = "https://pixe.la/v1/users"
 
 # # creating the user
-# user_params = {
-#     "token": config.TOKEN,
-#     "username": config.USERNAME,
-#     "agreeTermsOfService": "yes",
-#     "notMinor": "yes"
-# }
+user_params = {
+    "token": config.TOKEN,
+    "username": config.USERNAME,
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes"
+}
 # response = requests.post(url=PIXELA_ENDPOINT, json=user_params)
 # # created the user
+
+# Creating a graph
+graph_params = {
+    "id": "graph1",
+    "name": "Meditation",
+    "unit": "minutes",
+    "type": "float",
+    "color": "momiji"  # momoji == red
+}
+headers = {"X-USER-TOKEN": config.TOKEN}
+graph_response = requests.post(url=f"{PIXELA_ENDPOINT}/{config.USERNAME}/graphs", json=graph_params, headers=headers)
+print(graph_response.text)
+# created the graph
