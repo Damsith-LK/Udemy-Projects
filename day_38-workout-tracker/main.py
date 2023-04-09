6e872d8bffeb011d01d74d29ecc369aaf8ed680f
Damsith Wijekoon <damsith.wijekoon@Outlook.com>
2023-04-09 19:12:21 +0530

    Almost complete day 38

--- day_38-workout-tracker/main.py
+++ day_38-workout-tracker/main.py
@@ -2,11 +2,13 @@
 
 import config
 import requests
+import datetime
 
 GENDER = "male"
 END_POINT = "https://trackapi.nutritionix.com"
+
 exercise_params = {
-    "query": "did 100 push-ups in 15 minutes and did 6 sets of 4 pull-ups and cycled 500m",
+    "query": input("What exercises did you do: "),
     "gender": GENDER,
     "weight_kg": 37.5,
     "height_cm": 163.5,
@@ -19,4 +21,26 @@ exercise_headers = {
 
 exercise_response = requests.post(url=f"{END_POINT}/v2/natural/exercise", json=exercise_params,
                                   headers=exercise_headers)
-print(exercise_response.text)
+data = exercise_response.json()
+print(data)
+
+
+
+today_date = datetime.datetime.now().strftime("%d/%m/%Y")
+now_time = datetime.datetime.now().strftime("%X")
+
+sheet_header = {"Authorization": config.SHEETY_TOKEN}
+for exercise in data["exercises"]:
+    sheet_inputs = {
+        "workout": {
+            "date": today_date,
+            "time": now_time,
+            "exercise": exercise["name"].title(),
+            "duration": "{} mins".format(str(exercise["duration_min"])),
+            "calories": exercise["nf_calories"]
+        }
+    }
+
+    sheet_response = requests.post(config.SHEETY_ENDPOINT, json=sheet_inputs, headers=sheet_header)
+
+    print(sheet_response.text)
