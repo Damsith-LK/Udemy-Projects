69c0f74c5b8205f8f4107e298d4e7d9dfbf7ff8f
Damsith Wijekoon <damsith.wijekoon@Outlook.com>
2023-05-04 11:07:12 +0530

    Finish creating the apperance of the ui

--- day_37-habit-tracker/ui.py
+++ day_37-habit-tracker/ui.py
@@ -3,7 +3,9 @@ from tkinter import *
 # Got these colors from colorhunt.io
 BG = "#FF6D60"
 LABEL_COLOR = "#F7D060"
-BORDER_COLOR = "#98D8AA"
+BORDER_COLOR = "#F3E99F"
+IMAGE_BORDER_COLOR = "#98D8AA"
+BUTTON_PRESS_COLOR = "cyan"
 
 
 class GUI:
@@ -17,6 +19,7 @@ class GUI:
         image = PhotoImage(file="meditation.png")
         self.canvas = Canvas(width=450, height=475)
         self.image = self.canvas.create_image(225, 234, image=image)
+        self.canvas.config(highlightbackground=IMAGE_BORDER_COLOR, highlightthickness=10)
         self.canvas.grid(column=0, row=0)
 
         self.day_label = Label(text="Which day you want to update: ")
@@ -26,6 +29,7 @@ class GUI:
         self.day_entry = Entry(width=50)
         self.day_entry.config(highlightcolor=BORDER_COLOR, highlightthickness=3)
         self.day_entry.focus()
+        self.day_entry.insert(0, "Yesterday")
         self.day_entry.grid(column=1, row=1, columnspan=2)
 
         self.minutes_label = Label(text="Number of minutes you meditated (An int or a float is expected): ")
@@ -35,6 +39,10 @@ class GUI:
         self.minutes_entry = Entry(width=50)
         self.minutes_entry.config(highlightcolor=BORDER_COLOR, highlightthickness=3)
         self.minutes_entry.grid(column=1, row=2, columnspan=2)
+
+        self.button = Button(text="Submit")
+        self.button.config(relief="raised", width=45, activebackground=BUTTON_PRESS_COLOR)
+        self.button.grid(column=1, row=3)
 
         self.window.mainloop()
 
