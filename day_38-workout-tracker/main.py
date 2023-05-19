8a845be68875c7ffa3e5984199ddc4576546d036
Damsith Wijekoon <damsith.wijekoon@Outlook.com>
2023-05-19 19:49:56 +0530

    Create the UI appearance

--- day_38-workout-tracker/main.py
+++ day_38-workout-tracker/main.py
@@ -1,18 +1,27 @@
 # Here we are going to make a workout tracker program with nutritionnix API and also using google sheets
+# 2023/05/16 - Making a UI for this
 
+from tkinter import *
 import config
 import requests
 import datetime
 
+# Constants
+WINDOW_COLOR = "#3C486B"
+BORDER_COLOR = "#F0F0F0"
+TEXT_COLOR = "#F9D949"
+TOPIC_COLOR = "#F45050"
+HIGHLIGHT_COLOR = "#F0FF42"
+TEXT_FONT = ("Ariel", 11, "bold")
 GENDER = "male"
 END_POINT = "https://trackapi.nutritionix.com"
 
 exercise_params = {
     "query": input("What exercises did you do: "),
     "gender": GENDER,
-    "weight_kg": 37.5,
+    "weight_kg": 38,
     "height_cm": 163.5,
-    "age": 14
+    "age": 15
 }
 exercise_headers = {
     "x-app-id": config.NIX_ID,
@@ -70,3 +79,42 @@ for exercise in data["exercises"]:
     sheet_response = requests.post(config.SHEETY_ENDPOINT, json=sheet_inputs, headers=sheet_header)
 
     print(sheet_response.text)
+
+
+# --------------------------------- UI Setup ------------------------------------------ #
+# Creating the window
+root = Tk()
+root.config(bg=WINDOW_COLOR, pady=20, padx=20)
+root.title("Workout Tracker")
+
+# Topic
+topic_label = Label(text="Workout Tracker made using Python", fg=TOPIC_COLOR, font=("Times New Roman", 23, "underline"), bg=WINDOW_COLOR, pady=20)
+topic_label.grid(column=1, row=0)
+
+exercises_label = Label(text="Exercises you did: ", bg=WINDOW_COLOR, fg=TEXT_COLOR, font=TEXT_FONT)
+exercises_label.grid(column=0, row=1, sticky="e")
+
+exercises_entry = Entry(border=2, width=105, fg="black", highlightcolor=HIGHLIGHT_COLOR, highlightthickness=3)
+exercises_entry.grid(column=1, row=1, columnspan=2, pady=10)
+
+time_label = Label(text="Time (example input - 18:30): ", bg=WINDOW_COLOR, fg=TEXT_COLOR, font=TEXT_FONT)
+time_label.grid(column=0, row=2, pady=10, sticky="e")
+
+time_entry = Entry(border=2, width=105, fg="black", highlightcolor=HIGHLIGHT_COLOR, highlightthickness=3)
+time_entry.grid(column=1, row=2, columnspan=2, pady=10)
+
+# Date label and an Option Menu for choosing the date
+date_label = Label(text="Date: ", bg=WINDOW_COLOR, fg=TEXT_COLOR, font=TEXT_FONT)
+date_label.grid(column=0, row=3, sticky="e")
+selected_date = StringVar()
+dates = ["Today", "Yesterday", "Day Before Yesterday"]
+selected_date.set(dates[0])
+drop_box = OptionMenu(root, selected_date, *dates)
+drop_box.config(fg="blue", bg=HIGHLIGHT_COLOR, font=TEXT_FONT, activebackground=TOPIC_COLOR)
+drop_box.grid(column=1, row=3, sticky="w", pady=10)
+
+# The submit button
+submit_button = Button(text="Submit", font=TEXT_FONT, bg="#F97B22", activebackground="cyan")
+submit_button.grid(column=2, row=4)
+
+root.mainloop()
