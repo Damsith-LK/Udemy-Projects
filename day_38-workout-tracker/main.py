c5e3b71c19302ecc26d310531f41fe381b8025f4
Damsith Wijekoon <damsith.wijekoon@Outlook.com>
2023-05-20 10:23:41 +0530

    Successfully, finish creating an UI for Workout Tracker using Tkinter

--- day_38-workout-tracker/main.py
+++ day_38-workout-tracker/main.py
@@ -1,10 +1,79 @@
-# Here we are going to make a workout tracker program with nutritionnix API and also using google sheets
+# Here we are going to make a workout tracker program with nutritionix API and also using google sheets
 # 2023/05/16 - Making a UI for this
 
 from tkinter import *
 import config
 import requests
 import datetime
+
+GENDER = "male"
+END_POINT = "https://trackapi.nutritionix.com"
+
+# ----------------------- SUBMIT FUNCTION ------------------------- #
+
+def submit():
+
+    # Checking for faults in day entry
+    day = selected_date.get().lower()
+    if day == 'day before yesterday':
+        day = datetime.datetime.now() - datetime.timedelta(days=2)
+    elif day == "yesterday":
+        day = datetime.datetime.now() - datetime.timedelta(days=1)
+    else:
+        day = datetime.datetime.now()
+    day = day.strftime("%d/%m/%Y")
+
+    # Checking for faults in time entry
+    time_ = time_entry.get().lower()
+    if ":" not in time_ and time_ != "now":
+        quit("Invalid Input")
+    if time_ == "now":
+        time_ = datetime.datetime.now().strftime("%X")
+    else:
+        time_ += ":00"
+
+    exercises = exercises_entry.get()
+
+    # Getting relevant information about the workout from nutritionix API
+    exercise_params = {
+        "query": exercises,
+        "gender": GENDER,
+        "weight_kg": 38,
+        "height_cm": 163.5,
+        "age": 15
+    }
+    exercise_headers = {
+        "x-app-id": config.NIX_ID,
+        "x-app-key": config.NIX_KEY,
+    }
+
+    exercise_response = requests.post(url=f"{END_POINT}/v2/natural/exercise", json=exercise_params,
+                                      headers=exercise_headers)
+    data = exercise_response.json()
+    print(data)
+
+    # Sending the information to the Google Sheet via Sheety API
+    sheet_header = {"Authorization": config.SHEETY_TOKEN}
+
+    for exercise in data["exercises"]:
+        sheet_inputs = {
+            "workout": {
+                "date": day,
+                "time": time_,
+                "exercise": exercise["name"].title(),
+                "duration": "{} mins".format(str(exercise["duration_min"])),
+                "calories": exercise["nf_calories"]
+            }
+        }
+
+        sheet_response = requests.post(config.SHEETY_ENDPOINT, json=sheet_inputs, headers=sheet_header)
+
+        print(sheet_response.text)
+
+    # Destroying the window after submitting
+    root.destroy()
+
+# ----------------------------- SUBMIT FUNCTION END HERE --------------------------- #
 
 # Constants
 WINDOW_COLOR = "#3C486B"
@@ -13,73 +82,6 @@ TEXT_COLOR = "#F9D949"
 TOPIC_COLOR = "#F45050"
 HIGHLIGHT_COLOR = "#F0FF42"
 TEXT_FONT = ("Ariel", 11, "bold")
-GENDER = "male"
-END_POINT = "https://trackapi.nutritionix.com"
-
-exercise_params = {
-    "query": input("What exercises did you do: "),
-    "gender": GENDER,
-    "weight_kg": 38,
-    "height_cm": 163.5,
-    "age": 15
-}
-exercise_headers = {
-    "x-app-id": config.NIX_ID,
-    "x-app-key": config.NIX_KEY,
-}
-
-exercise_response = requests.post(url=f"{END_POINT}/v2/natural/exercise", json=exercise_params,
-                                  headers=exercise_headers)
-data = exercise_response.json()
-print(data)
-
-
-# ----------------------- FUNCTIONS START HERE ------------------------- #
-def get_date() -> str:
-    day = input("When did you do these exercises? (day before yesterday or yesterday or today): ").lower()
-    if day == 'day before yesterday':
-        day = datetime.datetime.now() - datetime.timedelta(days=2)
-    elif day == "yesterday":
-        day = datetime.datetime.now() - datetime.timedelta(days=1)
-    else:
-        day = datetime.datetime.now()
-
-    return str(day.strftime("%d/%m/%Y"))
-
-def get_time() -> str:
-    time = input("At what time of the day did you these glorious exercises (eg - 18:30 or now): ").lower()
-    if ":" not in time and time != "now":
-        quit("Invalid Input")
-    if time == "now":
-        time = datetime.datetime.now().strftime("%X")
-    else:
-        time += ":00"
-
-    return time
-# ----------------------------- FUNCTIONS END HERE --------------------------- #
-
-
-date = get_date()
-time = get_time()
-print(date, time)
-
-sheet_header = {"Authorization": config.SHEETY_TOKEN}
-
-for exercise in data["exercises"]:
-    sheet_inputs = {
-        "workout": {
-            "date": date,
-            "time": time,
-            "exercise": exercise["name"].title(),
-            "duration": "{} mins".format(str(exercise["duration_min"])),
-            "calories": exercise["nf_calories"]
-        }
-    }
-
-    sheet_response = requests.post(config.SHEETY_ENDPOINT, json=sheet_inputs, headers=sheet_header)
-
-    print(sheet_response.text)
-
 
 # --------------------------------- UI Setup ------------------------------------------ #
 # Creating the window
@@ -101,6 +103,7 @@ time_label = Label(text="Time (example input - 18:30): ", bg=WINDOW_COLOR, fg=TE
 time_label.grid(column=0, row=2, pady=10, sticky="e")
 
 time_entry = Entry(border=2, width=105, fg="black", highlightcolor=HIGHLIGHT_COLOR, highlightthickness=3)
+time_entry.insert(0, "now")
 time_entry.grid(column=1, row=2, columnspan=2, pady=10)
 
 # Date label and an Option Menu for choosing the date
@@ -113,8 +116,8 @@ drop_box = OptionMenu(root, selected_date, *dates)
 drop_box.config(fg="blue", bg=HIGHLIGHT_COLOR, font=TEXT_FONT, activebackground=TOPIC_COLOR)
 drop_box.grid(column=1, row=3, sticky="w", pady=10)
 
-# The submit button
-submit_button = Button(text="Submit", font=TEXT_FONT, bg="#F97B22", activebackground="cyan")
+# The submit button - using lambda function in order to input multiple commands at once
+submit_button = Button(text="Submit", font=TEXT_FONT, bg="#F97B22", activebackground="cyan", command=submit)
 submit_button.grid(column=2, row=4)
 
 root.mainloop()
